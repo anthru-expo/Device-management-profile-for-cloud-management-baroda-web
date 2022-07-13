cb2c558346c58e9659b62d4700d0b1d37a10b755
anthru-expo <neel.lict19@sot.pdpu.ac.in>
2022-07-13 15:04:06 +0530

    first commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+const About = () => {
+    return(
+        <div className="container">
+            <div className="py-4">
+                <h1> About</h1>
+                <p className="lead"> prototype 1</p>
+            </div>
+        </div>
+    );
+
+};
+
+export default About;
